ae6f1dfb774e95bc8f84484d6cca336fc3805396
Zetta <zettat123@gmail.com>
2018-02-11 19:57:17 +0800

    Optimize styles by antd

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import cx from 'classnames'
 import LoginForm from 'components/LoginForm'
 import VideoChat from 'components/VideoChat'
 import styles from './MainPage.scss'
 
 const MainPage = () => (
-  <div>
-    <div className={cx(styles.text)}>This is MainPage</div>
-    <Link to="/about">ABOUT</Link>
+  <div className={cx(styles.root)}>
     <LoginForm />
     <VideoChat />
   </div>
